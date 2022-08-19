f8274b0e43e5def6179410e89e0c34b8c929ba9c
Akbar Nurullayev <108784881+Nurullayev-Akbar@users.noreply.github.com>
2022-08-19 23:46:58 +0500

    bo'ldim

---
+++ main.js
@@ -0,0 +1,24 @@
+var dollor=9433.34;
+var yevro=10354.03;
+var bilet=500*dollor;
+var hotel=250*dollor;
+var travel=120*yevro;
+alert("Assalomu aleykum Alisher aka")
+var sum=bilet+hotel+travel;
+var a_sum=prompt("Alisher aka qancha puliz bor o'zi")-0;
+var nat1="Oq yo’l, Alisher!";
+var nat2="Alisher, ozgina sabr qilish kerak bo’lar ekan"
+// var chiq = document.querySelector(".h2")
+//   chiq.textContent=sum;
+
+if(a_sum >= sum)
+ {
+    var chiq = document.querySelector(".h2")
+    chiq.textContent=nat1;
+ }
+else
+ {
+    var chiq = document.querySelector(".h2")
+    chiq.textContent=nat2;
+ }
+
